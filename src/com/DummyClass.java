4f0ea1b41d2f3a22f2f7af9ba9ba2ba187f5fd0b
dhaval24 <dhavaldoshi24@gmail.com>
2016-04-18 16:12:12 -0500

    Initial edit

--- src/com/DummyClass.java
+++ src/com/DummyClass.java
@@ -4,4 +4,12 @@ package com;
  * Created by kushagrathapar on 4/18/16 at 1:56 PM.
  */
 public class DummyClass {
+
+    /**
+     * Edit by dhaval doshi
+     * @param args
+     */
+    public static void main(String[] args) {
+
+    }
 }
